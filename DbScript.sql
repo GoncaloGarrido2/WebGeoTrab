eb126efcf428bdb7841ffc0cbe502588eee3e0b4
Goncalo Garrido <Goncalosantosgarrido@gmail.com>
2023-06-11 19:53:26 +0100

    base de dados alterada

--- DbScript.sql
+++ DbScript.sql
@@ -9,3 +9,29 @@ create database AppAmigos
 	tablespace = ts_AppAmigos
 	encoding = 'UTF8'
 	
+CREATE EXTENSION postgis;
+
+CREATE TABLE Utilizador (
+    id SERIAL PRIMARY KEY,
+    Nome VARCHAR(255) NOT NULL,
+    DataNascimento DATE NOT NULL
+);
+
+CREATE TABLE Localizacao (
+    Id SERIAL PRIMARY KEY,
+    Ativo BOOLEAN NOT NULL,
+    DataAtual TIMESTAMP NOT NULL,
+    DataExpira TIMESTAMP NOT NULL,
+    Localizacao GEOMETRY(Point, 4326) NOT NULL
+);
+
+CREATE TABLE Notificacao (
+    id SERIAL PRIMARY KEY,
+    Enviado BOOLEAN NOT NULL
+);
+
+CREATE TABLE Mensagem (
+    id SERIAL PRIMARY KEY,
+    Titulo VARCHAR(255) NOT NULL,
+    Mensagem TEXT NOT NULL
+);
