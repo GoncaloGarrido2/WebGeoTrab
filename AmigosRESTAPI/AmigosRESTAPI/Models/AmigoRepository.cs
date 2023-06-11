3211c0f64ea704d278b02e0d936d2d23b6a45c52
Goncalo Garrido <Goncalosantosgarrido@gmail.com>
2023-06-11 22:02:50 +0100

    cRIACAO DO REST API

---
+++ AmigosRESTAPI/AmigosRESTAPI/Models/AmigoRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AmigosRESTAPI.Models {
+    public class AmigoRepository : IAmigoRepository {
+        public IEnumerable<Amigo> GetAll() {
+            return new Amigo[] {
+                new Amigo {IdAmigo = 1000, Nome = "Zeferino"},
+                new Amigo {IdAmigo = 1001, Nome = "Sebastiao" },
+                new Amigo {IdAmigo = 1002, Nome = "Valentim"},
+                new Amigo {IdAmigo = 1003, Nome = "Vilaça"}
+            };
+        }
+    }
+}
